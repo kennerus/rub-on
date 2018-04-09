d2c419d3a56dc6064b53400dd6a2808bc17c02f6
kavalar <apuc06@mail.ru>
2018-04-09 12:08:09 +0300

    footer, menu fix

--- js/global.js
+++ js/global.js
@@ -9,10 +9,12 @@ $(function() {
 
 	$(document).on('mouseenter', '.js_showCategoryList', function() {
 		$(this).find('.header__dropdown').slideDown(0).css('display', 'flex');
+		$(this).find('.header__link_category').addClass('header__link_hover');
 	});
 
 	$(document).on('mouseleave', '.js_showCategoryList', function() {
 		$(this).find('.header__dropdown').slideUp(0);
+		$(this).find('.header__link_category').removeClass('header__link_hover');
 	});
 
 	$(document).on('click', '.js_nextCategories', function() {
